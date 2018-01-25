fcc4f8982641cd2a7ac13b3704cb36720a59f8be
Eric Seastrand <eric@ericseastrand.com>
2018-01-25 09:38:54 -0600

    Create sql_formatter_defaults.user.js

---
+++ sql_formatter_defaults.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         
+// @namespace    https://www.bubbleup.net/
+// @version      0.1
+// @description  Sets sane defaults for SQL formatting
+// @author       Eric Seastrand
+// @match        http://www.dpriver.com/pp/sqlformat.htm
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/sql_formatter_defaults.user.js
+// ==/UserScript==
+
+function setSaneDefaults() {
+	// forEach method, could be shipped as part of an Object Literal/Module
+	var forEach = function (array, callback, scope) {
+	  for (var i = 0; i < array.length; i++) {
+	    callback.call(scope, i, array[i]); // passes back stuff we need
+	  }
+	};
+
+	var iterateSelector = function(selector, callback) {
+		var elements = document.querySelectorAll(selector);
+		forEach(elements, callback);
+	}
+
+	iterateSelector('#formatoptions select', function (index, value) {
+	    value.value="Unchanged";
+	});
+	iterateSelector('#formatoptions select[name="keywordcs"]', function (index, value) {
+	    value.value="Uppercase";
+	});
+	iterateSelector('#formatoptions select[name="andorunderwhere"], #formatoptions select[name="removelnbr"]', function (index, value) {
+		value.checked = true;
+	});
+
+	iterateSelector('#formatoptions select[name="dbvendor"]', function (index, value) {
+		value.value = 'mysql';
+	});
+	
+}
+
+setSaneDefaults();
