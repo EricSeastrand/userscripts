2e42a0906b817d1a735f70afbfe311a0329a588b
Eric Seastrand <eric@ericseastrand.com>
2018-01-25 10:24:55 -0600

    Update sql_formatter_defaults.user.js

--- sql_formatter_defaults.user.js
+++ sql_formatter_defaults.user.js
@@ -9,52 +9,52 @@
 // ==/UserScript==
 
 function setSaneDefaults() {
-	// forEach method, could be shipped as part of an Object Literal/Module
-	var forEach = function (array, callback, scope) {
-	  for (var i = 0; i < array.length; i++) {
-	    callback.call(scope, i, array[i]); // passes back stuff we need
-	  }
-	};
+    // forEach method, could be shipped as part of an Object Literal/Module
+    var forEach = function (array, callback, scope) {
+      for (var i = 0; i < array.length; i++) {
+        callback.call(scope, i, array[i]); // passes back stuff we need
+      }
+    };
 
-	var iterateSelector = function(selector, callback) {
-		var elements = document.querySelectorAll(selector);
-		forEach(elements, callback);
-	};
+    var iterateSelector = function(selector, callback) {
+        var elements = document.querySelectorAll(selector);
+        forEach(elements, callback);
+    };
 
-	iterateSelector('#formatoptions select', function (index, element) {
-	    element.value="Unchanged";
-	});
-	iterateSelector('#formatoptions select[name="keywordcs"]', function (index, element) {
-	    element.value="Uppercase";
-	});
-	iterateSelector('#formatoptions select[name="removelnbr"]', function (index, element) {
-		element.checked = true;
-	});
-	iterateSelector('#formatoptions input[name="andorunderwhere"]', function (index, element) {
-		element.checked = true;
-	});
+    iterateSelector('#formatoptions select', function (index, element) {
+        element.value="Unchanged";
+    });
+    iterateSelector('#formatoptions select[name="keywordcs"]', function (index, element) {
+        element.value="Uppercase";
+    });
+    iterateSelector('#formatoptions select[name="removelnbr"]', function (index, element) {
+        element.checked = true;
+    });
+    iterateSelector('#formatoptions input[name="andorunderwhere"]', function (index, element) {
+        element.checked = true;
+    });
 
-	iterateSelector('#formatoptions input[name="maxlenincm"]', function (index, element) {
-		element.value = 999;
-	});
+    iterateSelector('#formatoptions input[name="maxlenincm"]', function (index, element) {
+        element.value = 999;
+    });
+    
 
-
-	iterateSelector('select[name="dbvendor"]', function (index, element) {
-		element.value = 'mysql';
-	});
-
+    iterateSelector('select[name="dbvendor"]', function (index, element) {
+        element.value = 'mysql';
+    });
+    
     iterateSelector('#ioutputsql', function (index, element) {
-		element.style.height = '1000px';
-	});
+        element.style.height = '1000px';
+    });
 
-	iterateSelector('#inputsql', function (index, element) {
-		element.addEventListener('blur', function(){
-			SQLFMT.format();
-		});
-	});
-
-
-
+    iterateSelector('#inputsql', function (index, element) {
+        element.addEventListener('blur', function(){
+            SQLFMT.format();
+        })
+    });
+    
+   
+    
 }
 
 setSaneDefaults();
