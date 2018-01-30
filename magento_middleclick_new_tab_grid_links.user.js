fab26b93153eb3c9a53bdef2c6546b27301835b1
Eric Seastrand <eric@ericseastrand.com>
2018-01-30 09:41:20 -0600

    Update magento_middleclick_new_tab_grid_links.user.js

--- magento_middleclick_new_tab_grid_links.user.js
+++ magento_middleclick_new_tab_grid_links.user.js
@@ -29,7 +29,7 @@
     iterateSelector('.grid table.data tr', function(el) {
         var url = el.title;
         el.addEventListener('click', function(e){
-            if(e.ctrlKey || e.which = 2){
+            if(e.ctrlKey || e.which == 2){
                 window.open(url);
             }
             console.log(e, el, url);
