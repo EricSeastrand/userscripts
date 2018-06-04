c0f3628dbebc14fdc07bbf6bb699bf91ca702433
Eric Seastrand <eric@ericseastrand.com>
2018-06-04 09:47:48 -0500

    Create pushservice_db_transfer_helper.user.js

---
+++ bubbleup/pushservice_db_transfer_helper.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         PS DB transfer helper
+// @namespace    https://www.bubbleup.net
+// @version      0.1
+// @description  Builds DB transfer commands/queries for you
+// @author       Eric Seastrand
+// @match        https://push-service.busites.com/index.php?r=sites/view&id=*
+// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/db_magento_queries.js
+// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/siteview_extras.js
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/bubbleup/pushservice_db_transfer_helper.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // No code here for now. It all loads dynamically from our dev server.
+})();
