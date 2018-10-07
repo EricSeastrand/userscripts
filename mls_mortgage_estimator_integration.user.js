1127aba41ccbbd2007b0f3b5c943b6ae47e48ce4
Eric Seastrand <ericseastrand@gmail.com>
2018-10-07 16:22:15 -0500

    Create mls_mortgage_estimator_integration.user.js

---
+++ mls_mortgage_estimator_integration.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         HARMLS Mortgage Estimator Integration
+// @namespace    https://www.ericseastrand.com/
+// @version      0.1
+// @description  Pre populates fields in the mortgage estimator using data from a MLS listing
+// @author       Eric Seastrand
+// @include      https://www.mortgagecalculator.org/
+// @include      https://matrix.harmls.com/Matrix/Public/Portal.aspx
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/mls_mortgage_estimator_integration.user.js
+// ==/UserScript==
+
+console.log("loaded");
