69b7033f59e1940ca4cb07639b8badeebb804a8d
Eric Seastrand <ericseastrand@gmail.com>
2018-10-23 14:33:21 -0500

    Create pushservice_db_transfer_shortcut.user.js

---
+++ bubbleup/pushservice_db_transfer_shortcut.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         PS DB transfer shortcut
+// @namespace    https://www.bubbleup.net
+// @version      0.1
+// @description  Builds DB transfer commands/queries for you, based on your selections in the deployment list
+// @author       Eric Seastrand
+// @match        https://push-service.busites.com/index.php?r=deployments/admin*
+// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/db_magento_queries.js
+// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/siteview_extras.js
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/bubbleup/pushservice_db_transfer_shortcut.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+   // No code here for now. It all loads dynamically from our dev server. 
+console.log("Alive!");
+})();
