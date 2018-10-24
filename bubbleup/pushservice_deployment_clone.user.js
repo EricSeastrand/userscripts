2a57bf903d7ca85ef89d368180ef3206b12c1a35
Eric Seastrand <ericseastrand@gmail.com>
2018-10-24 07:40:02 -0500

    Create pushservice_deployment_clone.user.js

---
+++ bubbleup/pushservice_deployment_clone.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         PS Deployment Clone Query Builder
+// @namespace    https://www.bubbleup.net
+// @version      0.1
+// @description  Builds deployment clone commands/queries for you
+// @author       Eric Seastrand
+// @match        https://push-service.busites.com/index.php?r=deployments/view&id=*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/bubbleup/pushservice_deployment_clone.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // No code here for now. It all loads dynamically from our dev server.
+})();
