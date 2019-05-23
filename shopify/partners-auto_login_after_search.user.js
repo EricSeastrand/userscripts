649058735818be4df67f30e70dec70f192d24484
Eric Seastrand <ericseastrand@gmail.com>
2019-05-23 12:15:48 -0500

    Create partners-auto_login_after_search.user.js

---
+++ shopify/partners-auto_login_after_search.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Shopify: Partners Panel Auto-Login
+// @namespace    https://www.ericseastrand.com/
+// @version      0.1
+// @description  Automatically clicks the Login button for you, after searching for a store in the Shopify Partners Panel, assuming there is only 1 search result.
+// @author       Eric Seastrand
+// @include      https://partners.shopify.com/*/development_stores*q=*
+// @include      https://partners.shopify.com/*/managed_stores*q=*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/shopify/partners-auto_login_after_search.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    console.log("Loaded Script");
+
+
+})();
