13485b92315a5f3e9e036950edeed71834204876
Eric Seastrand <ericseastrand@gmail.com>
2019-05-24 14:27:07 -0500

    Update soundscan_bulkeditor.user.js

--- shopify/soundscan_bulkeditor.user.js
+++ shopify/soundscan_bulkeditor.user.js
@@ -8,66 +8,86 @@
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/shopify/soundscan_bulkeditor.user.js
 // ==/UserScript==
-
 (function() {
     'use strict';
 
-
-  function buildBulkEditorUrlForSoundscan() {
-    const metaFieldsToEdit = [
-      'metafields.soundscan.report:string',
-      'metafields.soundscan.release-date:string',
-      'metafields.soundscan.media-type:string',
-      'variants.barcode'
-    ];
-
-    return buildBulkEditorUrl(metaFieldsToEdit);
-  }
-
-  function buildBulkEditorUrl(metaFieldsToEdit) {
-    const originalQueryString = window.location.search.replace(/^\??/, ''); // Trim the leading ? from the URL
-    const params = new URLSearchParams(originalQueryString);
-
-    params.set('edit', metaFieldsToEdit.join(','));
-
-    const newQueryString = params.toString();
-
-    const fullEditorUrl = window.location.href.replace(originalQueryString, newQueryString);
-
-    return fullEditorUrl;
-  }
-
-  function addBubbleUpShortcuts(items) {
-    const shortcutContainer = $('<div>').addClass('bu-shortcuts');
-
-    shortcutContainer.append('<span>BU Shortcuts</span>');
-
-    jQuery('#next-label--columns').closest('.next-card__section').prepend(shortcutContainer);
-
-    items.forEach(function(item) {
-      const link = $('<a class="bu-shortcut-link">').text(item.label);
-
-      if(item.href) {
-        link.attr('href', item.href);
-        link.click(function(){ window.location.href = item.href; });
-      }
-
-      if(item.onRender) {
-        item.onRender(link);
-      }
-
-      link.appendTo(shortcutContainer);
+    runCodeWhenElementPresent('#resource_table', function() {
+        addBubbleUpShortcuts([{
+            'label': 'Soundscan Fields',
+            'href': buildBulkEditorUrlForSoundscan()
+        }]);
     });
 
-  }
 
-  addBubbleUpShortcuts([
-    {
-      'label' : 'Soundscan Fields',
-      'href'  : buildBulkEditorUrlForSoundscan()
+    function buildBulkEditorUrlForSoundscan() {
+        const metaFieldsToEdit = [
+            'metafields.soundscan.report:string',
+            'metafields.soundscan.release-date:string',
+            'metafields.soundscan.media-type:string',
+            'variants.barcode'
+        ];
+
+        return buildBulkEditorUrl(metaFieldsToEdit);
     }
-  ]);
 
+    function buildBulkEditorUrl(metaFieldsToEdit) {
+        const originalQueryString = window.location.search.replace(/^\??/, ''); // Trim the leading ? from the URL
+        const params = new URLSearchParams(originalQueryString);
 
+        params.set('edit', metaFieldsToEdit.join(','));
+
+        const newQueryString = params.toString();
+
+        const fullEditorUrl = window.location.href.replace(originalQueryString, newQueryString);
+
+        return fullEditorUrl;
+    }
+
+    function addBubbleUpShortcuts(items) {
+        const shortcutContainer = $('<div>').addClass('bu-shortcuts');
+
+        shortcutContainer.append('<span>BU Shortcuts</span>');
+
+        jQuery('#next-label--columns').closest('.next-card__section').prepend(shortcutContainer);
+
+        items.forEach(function(item) {
+            const link = $('<a class="bu-shortcut-link">').text(item.label);
+
+            if (item.href) {
+                link.attr('href', item.href);
+                link.click(function() {
+                    window.location.href = item.href;
+                });
+            }
+
+            if (item.onRender) {
+                item.onRender(link);
+            }
+
+            link.appendTo(shortcutContainer);
+        });
+
+    }
+
+    async function runCodeWhenElementPresent(elementSelector, codeToRun) {
+        while (1) {
+            const element = document.querySelector(elementSelector);
+
+            if (element) {
+                console.log("Found element at", new Date());
+                codeToRun();
+                break;
+            }
+
+            //console.log("Wait for next frame started at", new Date());
+            await nextFrame();
+        }
+    }
+
+    async function nextFrame() {
+        return new Promise(resolve => {
+            requestAnimationFrame(resolve)
+        })
+    }
 
 })();
