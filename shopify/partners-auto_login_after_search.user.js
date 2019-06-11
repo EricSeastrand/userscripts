a944ca119c77b2edee09386b852710b1e8c6e2c2
Eric Seastrand <ericseastrand@gmail.com>
2019-06-11 10:02:40 -0500

    Now works with the new Polaris-based partners panel

--- shopify/partners-auto_login_after_search.user.js
+++ shopify/partners-auto_login_after_search.user.js
@@ -4,24 +4,23 @@
 // @version      0.1
 // @description  Automatically clicks the Login button for you, after searching for a store in the Shopify Partners Panel, assuming there is only 1 search result.
 // @author       Eric Seastrand
-// @include      https://partners.shopify.com/*/development_stores*q=*
-// @include      https://partners.shopify.com/*/managed_stores*q=*
+// @include      https://partners.shopify.com/*/stores*search_value=*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/shopify/partners-auto_login_after_search.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
-    runCodeWhenElementPresent('.ui-data-table__fixed-wrapper, .ui-resource-list', handleAutomaticLogin);
+
+    runCodeWhenElementPresent('.store-list-item', handleAutomaticLogin);
 
 })();
 
 
 function handleAutomaticLogin() {
-    const loginButtons = document.querySelectorAll('a.ui-resource-list-item__action[href$="/login"], .ui-data-table__fixed-wrapper a[href$="/login"]');
+    const loginButtons = document.querySelectorAll('.store-list-item__actions a[href$="/login_development"], .store-list-item__actions a[href$="/login_managed"]');
 
-    
+
     if(loginButtons.length > 1) {
         console.log("There is more than 1 login button, so you need to click it yourself. Auto-login won't happen");
         return;
@@ -38,7 +37,7 @@ function handleAutomaticLogin() {
 async function runCodeWhenElementPresent(elementSelector, codeToRun) {
 	while(1) {
 		const element = document.querySelector(elementSelector);
-		
+
 		if(element) {
 			console.log("Found element at", new Date());
 			codeToRun();
