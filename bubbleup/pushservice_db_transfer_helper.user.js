c5b81feae235aa7dd6ed7fa490540b6924422dc3
Eric Seastrand <ericseastrand@gmail.com>
2019-10-01 12:57:38 -0500

    Update pushservice_db_transfer_helper.user.js

--- bubbleup/pushservice_db_transfer_helper.user.js
+++ bubbleup/pushservice_db_transfer_helper.user.js
@@ -5,13 +5,54 @@
 // @description  Builds DB transfer commands/queries for you
 // @author       Eric Seastrand
 // @match        https://push-service.busites.com/index.php?r=sites/view&id=*
-// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/db_magento_queries.js
-// @require      https://pweb-utils.7.dev.bubbleup.com/pushservice/siteview_extras.js
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/willcodeforfood/userscripts/master/bubbleup/pushservice_db_transfer_helper.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    // No code here for now. It all loads dynamically from our dev server.
+    
+loadScript('https://pweb-utils.7.dev.bubbleup.com/pushservice/db_magento_queries.js');
+loadScript('https://pweb-utils.7.dev.bubbleup.com/pushservice/siteview_extras.js');
+
+function loadStylesheet(url) {
+        var head = document.getElementsByTagName('HEAD')[0];  
+  
+        // Create new link Element 
+        var link = document.createElement('link'); 
+  
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = url;  
+  
+        // Append link element to HTML head 
+        head.appendChild(link);  
+}
+
+function loadScript(url, callback){
+
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState){  //IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function(){
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
 })();
